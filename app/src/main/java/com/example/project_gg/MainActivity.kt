718a47b6cc80a4e3b67805ef9805487e8078cb29
CaptGuto <tadenafi@gmail.com>
2023-12-22 10:53:58 +0300

    First

--- app/src/main/java/com/example/project_gg/MainActivity.kt
+++ app/src/main/java/com/example/project_gg/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //this is a new comment
+
+        //this is the second
     }
 }
